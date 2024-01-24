e81770f85574be9b0dc2f10786a95d0dba8af8e5
marc135169 <marc.tetart2511@gmail.com>
2024-01-24 15:02:46 +0100

    Initial commit

---
+++ Source/SM_Duality/SM_DualityGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "SM_DualityGameModeBase.h"
+
---
+++ Source/SM_Duality/SM_DualityGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SM_DualityGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SM_DUALITY_API ASM_DualityGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
